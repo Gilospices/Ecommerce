2b5f32c76f244787342dc2ae699dedd64f88c7ed
user 4 <user 4@GANIU-ONIKOYI>
2020-01-29 10:36:56 +0100

    Api assignment

---
+++ Ecommerce/Api/GiloController1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ecommerce.Data;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Ecommerce.Api
+{
+    [Route("api/[controller]")]
+    public class GiloController1 : Controller
+    {
+        private readonly Ecommerce.Data.ApplicationDbContext _context;
+
+        public GiloController1(Ecommerce.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("MainCategoryById")]
+        public IActionResult GetMainCategoryById([FromQuery]int id)
+        {
+            var maincategory = _context.MainCategories.FirstOrDefault(x => x.Id == id);
+            if (maincategory != null)
+            {
+                return Ok(maincategory);
+
+            }
+            else
+            {
+                return NotFound("No record Found");
+            };
+            //return $"your name is {name} and ur ID is : {id}";
+        }
+
+        [HttpGet("SubCategoryById")]
+        public IActionResult GetSubCategoryById([FromQuery]int id)
+        {
+            var subcategory = _context.SubCategories.FirstOrDefault(x => x.Id == id);
+            if (subcategory != null)
+            {
+                return Ok(subcategory);
+
+            }
+            else
+            {
+                return NotFound("No record Found");
+            };
+            //return $"your name is {name} and ur ID is : {id}";
+        }
+
+
+        [HttpGet("MainCategoryALL")]
+        public List<MainCategory> MainCategories()
+        {
+            var maincategory = _context.MainCategories.ToList();
+            
+            return maincategory;
+
+        }
+
+        [HttpGet("SubCategoryALL")]
+        public List<SubCategory> subCategories()
+        {
+            var subcategories = _context.SubCategories.ToList();
+
+            return subcategories;
+
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Ecommerce/Api/ValuesController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ecommerce.Data;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Ecommerce.Api
+{
+    public class Valuesviewmodel
+    {
+        public string ActualAddress { get; set; }
+    }
+
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("ByName")]
+        public string Get([FromQuery]int id, [FromQuery] string name)
+        {
+
+            return $"your name is {name} and ur ID is : {id}";
+        }
+
+
+        [HttpGet("AddressById")]
+        public IActionResult GetAddressById([FromQuery]int id)
+        {
+            var listofaddrsss = new List<Address>
+            {
+                new Address
+                {
+                    Id=1,
+                    ActualAddress=" no 5 john",
+                },
+                new Address
+                {
+                    Id=2,
+                    ActualAddress=" no 6 mushin",
+                },
+                 new Address
+                {
+                    Id=2,
+                    ActualAddress=" no 10 oshodi",
+                },
+            };
+
+
+            var requestedaddress = listofaddrsss.FirstOrDefault(x => x.Id == id);
+            if (requestedaddress!=null)
+            {
+            return Ok(requestedaddress);
+
+            }
+            else
+            {
+                return NotFound("No record Found");
+             };
+            //return $"your name is {name} and ur ID is : {id}";
+        }
+
+
+        //[HttpGet("AddressByAll")]
+        //public IActionResult GetAddressById([FromQuery]int id)
+        //{
+        //    var listofaddrsss = new List<Address>
+        //    {
+        //        new Address
+        //        {
+        //            Id=1,
+        //            ActualAddress=" no 5 john",
+        //        },
+        //        new Address
+        //        {
+        //            Id=2,
+        //            ActualAddress=" no 6 mushin",
+        //        },
+        //         new Address
+        //        {
+        //            Id=3,
+        //            ActualAddress=" no 10 oshodi",
+        //        },
+        //    };
+
+
+        //    var data = listofaddrsss.Select(x => new Valuesviewmodel { ActualAddress = x.ActualAddress }).ToList();
+
+        //    return Ok( data);
+        //    //return $"your name is {name} and ur ID is : {id}";
+        //}
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
